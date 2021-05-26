967eb1f9b347f971880026eb384e7b857269bb53
claud-car <claudiocarchietti@yahoo.it>
2021-05-26 17:40:54 +0200

    update terminato

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12"></div>
+        <a href="{{route('admin.posts.create')}}">Nuovo Post</a>
+    </div>
+    <div class="row justify-content-center">
+    @foreach ($posts as $post)
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header">{{ $post->title }}</div>
+
+                <div class="card-body">
+                    {{$post->content}}
+                    <div>          
+                        <a href="{{route('admin.posts.edit', ['post' => $post->id])}}">Edit</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    </div>
+</div>
+
+
+@endsection
